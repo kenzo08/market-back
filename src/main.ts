e1ed6351f039b70199ea1ad0dc6e8ac06fab7e19
Bekhrooz Abdoovakhidov <behruzabduvahidov@gmail.com>
2025-07-02 08:42:40 +0500

    added auth with no worked email smtp

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: process.env.FRONTEND_ORIGIN,
+    origin: process.env.FRONTEND_URL,
   });
   app.setGlobalPrefix('api');
 
